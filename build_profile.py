a66116ae6777f0130bb678d8cebe28af4126e830
YIMIN ZHONG <yzz0225@auburn.edu>
2025-06-08 11:57:12 -0500

    Update build_profile.py
    add try ... except to avoid failure

--- build_profile.py
+++ build_profile.py
@@ -34,18 +34,17 @@ def replace_chunk(content, marker, chunk, inline=False):
 if __name__ == '__main__':
     readme = root / 'README.md'
     readme_contents = readme.open().read()
-    entries = fetch_rss_feed('https://api.quantamagazine.org/feed/')
-
-    print('Recent Posts on Quanta-Magazine\n')
-    entries_md = '\n'.join(
-        ['* <a href="{link}">{title}</a> - {published}'.format(**entry) for entry in entries]
-    )
-
-    rewritten = replace_chunk(readme_contents, 'quanta', entries_md)
-
-    readme.open('w').write(rewritten)
-
-
+    try:
+        entries = fetch_rss_feed('https://api.quantamagazine.org/feed/')
+        print('Recent Posts on Quanta-Magazine\n')
+        entries_md = '\n'.join(
+            ['* <a href="{link}">{title}</a> - {published}'.format(**entry) for entry in entries]
+        )
+    
+        rewritten = replace_chunk(readme_contents, 'quanta', entries_md)
+        readme.open('w').write(rewritten)
+    except:
+        pass
     # readme_contents = readme.open().read()
     # entries = fetch_rss_feed('https://www.siam.org/rss-feed/siam-news/current-issue')
     
@@ -60,16 +59,20 @@ if __name__ == '__main__':
     # readme.open('w').write(rewritten)
 
     readme_contents = readme.open().read()
-    entries = fetch_rss_feed('http://export.arxiv.org/rss/math.NA', 0)
+
+    try:
+        entries = fetch_rss_feed('http://export.arxiv.org/rss/math.NA', 0)
+        print('Recent Posts on Arxiv Math.NA\n')
     
-    print('Recent Posts on Arxiv Math.NA\n')
-
-    entries_md = '\n'.join(
-        [('* <a href="{link}">{title}</a> - {published}' if len(entry['published'])>0 else '* <a href="{link}">{title}</a>').format(**entry) for entry in entries]
-    )
-
-    # print(entries_md)
-    rewritten = replace_chunk(readme_contents, 'arxiv-math-na', entries_md)
-    readme.open('w').write(rewritten)
-
+        entries_md = '\n'.join(
+            [('* <a href="{link}">{title}</a> - {published}' if len(entry['published'])>0 else '* <a href="{link}">{title}</a>').format(**entry) for entry in entries]
+        )
+    
+        # print(entries_md)
+        rewritten = replace_chunk(readme_contents, 'arxiv-math-na', entries_md)
+        readme.open('w').write(rewritten)
+        
+    except:
+        pass
+    
     print('Done!')
